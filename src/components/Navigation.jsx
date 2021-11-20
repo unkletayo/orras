6583a81679cdbf817e721d37a054d8c327ef546c
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2021-11-20 04:09:49 +0100

    Starting
    Signed-off-by: Adetayo Akinsanya <akinsanyaadetayo@gmail.com>

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+const Navigation = () => {
+  return (
+    <div>
+      <Link href="#idf">
+        <a>Who we are</a>
+      </Link>
+      <Link href="#idf">
+        <a>Who we are</a>
+      </Link>{' '}
+      <Link href="#idf">
+        <a>Who we are</a>
+      </Link>{' '}
+      <Link href="#idf">
+        <a>Who we are</a>
+      </Link>{' '}
+      <Link href="#idf">
+        <a>Who we are</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Navigation
