46b828d6dd20ce88bbf87b6cf44bb30aec426785
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2021-12-27 08:56:00 +0100

    Improve

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function SEO({
-  description = 'Make The World Listen',
-  author = 'Orras',
+  description = 'Dedicated to promoting ambitious musical talents to a larger audience across the world.',
+  author = '@orras_ent',
+  country="NG",
   meta,
-  title = 'Orras Landing Page',
+  title = 'Orras Enterntainment Website',
+  keywords= "Orras Entertainment, music label, record label",
+  site_name="orras.com"
 }) {
   const metaData = [
     {
       name: `description`,
       content: description,
+    },
+    {
+      name: `keywords`,
+      content: description,
+    },
+    {
+      property: `og:site_name`,
+      content: site_name,
     },
     {
       property: `og:title`,
@@ -34,11 +45,15 @@ export default function SEO({
     },
     {
       name: `twitter:title`,
-      content: title,
+      content: site_name,
     },
     {
       name: `twitter:description`,
       content: description,
+    },
+    {
+      name: `twitter:app:country`,
+      content: country,
     },
   ].concat(meta);
   
